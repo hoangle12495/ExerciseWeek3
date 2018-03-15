12d5f0a51471e54d190cf2514a4b4ac3c4158a36
hoanglee <hoangle12495@gmail.com>
2018-03-15 10:35:29 +0700

    Complete

---
+++ app/src/main/java/com/ngocdiem/exercise3/Main2Activity.java
@@ -0,0 +1,51 @@
+package com.ngocdiem.exercise3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class Main2Activity extends AppCompatActivity {
+    TextView txtMovieName,txtSumarize,txtRealeaseDate;
+    RatingBar ratingBar;
+    ImageView imgMovie;
+    Profile profile;
+    ImageButton imgPlay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        txtMovieName = findViewById(R.id.txtMovieName);
+        txtSumarize = findViewById(R.id.txtSumarize);
+        txtRealeaseDate = findViewById(R.id.txtRealeaseDate);
+        ratingBar = findViewById(R.id.ratingBar);
+        imgMovie = findViewById(R.id.imgMovie);
+        imgPlay = findViewById(R.id.imgPlay);
+        ratingBar.setMax(5);
+        ratingBar.setFocusable(false);
+        Intent intent = getIntent();
+        int position = intent.getIntExtra("Position",0);
+        profile = MainActivity.moviesList.get(position);
+        Picasso.with(this).load("https://image.tmdb.org/t/p/w500"+profile.getBackdropPath()).resize(350, 200).into(imgMovie);
+        txtMovieName.setText(profile.getTitle());
+        txtSumarize.setText(profile.getOverview());
+        txtRealeaseDate.setText(profile.getReleaseDate());
+        ratingBar.setRating(profile.getVoteAverage().floatValue()/2);
+        txtSumarize.setMovementMethod(new ScrollingMovementMethod());
+        if(profile.getVideo() == true)
+        {
+            imgPlay.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            imgPlay.setVisibility(View.INVISIBLE);
+        }
+    }
+}
